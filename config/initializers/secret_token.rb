3ccf8b2597628462764377e5398559b58feead03
Bjoern Keucher <bjoern.keucher@web.de>
2014-03-03 13:57:19 +0100

    Project Bootstrap

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+BrewReport::Application.config.secret_key_base = '01f1f9fac1306d0162a67e7ec1b2053cb865e1eefa5c2f772cc4a65a7ec1a1aaa00d9d07650e868bcdd991f36de2f7ee633620940fc3c4fa94cd96ccf8235a6f'
