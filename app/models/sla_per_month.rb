e6a73d914cbdf6acffc3c40342b28ed4fc4853fc
Phakx <Bjoern.Keucher@web.de>
2014-03-09 22:19:18 +0100

    little method for filtering Sla's

--- app/models/sla_per_month.rb
+++ app/models/sla_per_month.rb
@@ -1,4 +1,8 @@
 class SlaPerMonth < ActiveRecord::Base
   belongs_to :customer
   has_many :sla_per_days
+
+  def retrieve_by_month_and_year(month, year)
+    self.where('month = ? AND year = ?', month, year)
+  end
 end
