dd276fcc58383e889fecf436a41b145fd2ce6494
Bjoern Keucher <bjoern.keucher@web.de>
2014-03-12 18:24:05 +0100

    parse Jasperxml and write downtimes to database (not sla_per_month conn yet)

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -3,8 +3,19 @@ module ApplicationHelper
   require 'open-uri'
   require 'nokogiri'
 
-  def import_downtimes_from_icinga(url, username, password)
-    jasper_xml = Nokogiri::XML(open(url, :http_basic_authentication=>[username, password]))
+  def self.import_downtimes_from_icinga(url, username, password)
 
+    jasper_xml = Nokogiri::XML(open(url, :http_basic_authentication=>[username, password])) do |config|
+      config.strict.nonet
+    end
+    log_entries = jasper_xml.xpath("//log_entry")
+    log_entries.each do |entry|
+      downtime = Downtime.new
+      downtime.start = Time.at(entry.xpath("start_time_timestamp").text.to_i).to_datetime
+      downtime.end = Time.at(entry.xpath("end_time_timestamp").text.to_i).to_datetime
+      downtime.downtimeType = entry.xpath("entry_type").text
+      downtime.comment = entry.xpath("state_information").text
+      downtime.save
+    end
   end
 end
