ad4fa14103be3fb9c3c3477c1e8c25d49984c358
Phakx <Bjoern.Keucher@web.de>
2014-03-16 21:04:12 +0100

    Moving methods around

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -19,10 +19,54 @@ module ApplicationHelper
     end
   end
 
+  def self.find_or_create_sla_per_day(day, month, year)
+    logger = Logger.new(STDOUT)
+    sla_per_day = lambda { |sla_per_month|
+      daily_sla = SlaPerDay.new
+      daily_sla.day=day
+      daily_sla.sla_per_month = sla_per_month
+      daily_sla.save
+      return daily_sla
+    }
+
+    logger.debug "Trying to find Sla_per_day object for #{day} / #{month} / #{year}"
+    sla_per_month = SlaPerMonth.retrieve_by_month_and_year(month, year)
+    if sla_per_month.nil?
+
+      logger.debug 'no matching month object found creating new Month and day object'
+      sla_per_month_new = SlaPerMonth.new
+      sla_per_month_new.month= month
+      sla_per_month_new.year = year
+
+      if sla_per_month_new.save
+        return sla_per_day.call(sla_per_month_new)
+      else
+        return nil
+      end
+
+    else
+
+      logger.debug 'found sla_per_month object fetching day objects'
+      sla_per_days = SlaPerDay.retrieve_all_by_sla_per_month(sla_per_month.id)
+      sla_per_days.each do |daily_sla|
+        logger.debug 'searching for persisted matching SLA object'
+        logger.debug "ID=  #{daily_sla.id} DAY= #{daily_sla.day} expected day= #{day}"
+        if daily_sla.id != '' && (daily_sla.day == day)
+          logger.debug 'found match'
+          return daily_sla
+        end
+      end
+      logger.debug 'No match found creating new SlaPerDay and persisting'
+      return sla_per_day.call(sla_per_month)
+
+    end
+
+  end
+
   def self.create_new_downtime(entry, logger)
     downtime = Downtime.new
     start_datetime = Time.at(entry.xpath('start_time_timestamp').text.to_i).to_datetime
-    sla_per_day = SlaPerMonthsHelper.find_or_create_sla_per_day(start_datetime.day, start_datetime.month, start_datetime.year)
+    sla_per_day = find_or_create_sla_per_day(start_datetime.day, start_datetime.month, start_datetime.year)
     logger.debug "Using #{sla_per_day} for downtime: #{downtime}"
     downtime.sla_per_day = sla_per_day
     downtime.start = start_datetime
@@ -31,6 +75,5 @@ module ApplicationHelper
     downtime.comment = entry.xpath('state_information').text
     downtime.save
   end
-
-  private :self.create_new_downtime
+  private :self.create_new_downtime, :self.find_or_create_sla_per_day
 end
--- app/helpers/sla_per_months_helper.rb
+++ app/helpers/sla_per_months_helper.rb
@@ -1,46 +1,4 @@
 module SlaPerMonthsHelper
 
-  def self.find_or_create_sla_per_day(day, month, year)
-    logger = Logger.new(STDOUT)
-    sla_per_day = lambda { |sla_per_month|
-      daily_sla = SlaPerDay.new
-      daily_sla.day=day
-      daily_sla.sla_per_month = sla_per_month
-      daily_sla.save
-      return daily_sla
-    }
 
-    logger.debug "Trying to find Sla_per_day object for #{day} / #{month} / #{year}"
-    sla_per_month = SlaPerMonth.retrieve_by_month_and_year(month, year)
-    if sla_per_month.nil?
-
-      logger.debug 'no matching month object found creating new Month and day object'
-      sla_per_month_new = SlaPerMonth.new
-      sla_per_month_new.month= month
-      sla_per_month_new.year = year
-
-      if sla_per_month_new.save
-        return sla_per_day.call(sla_per_month_new)
-      else
-        return nil
-      end
-
-    else
-
-      logger.debug 'found sla_per_month object fetching day objects'
-      sla_per_days = SlaPerDay.retrieve_all_by_sla_per_month(sla_per_month.id)
-      sla_per_days.each do |daily_sla|
-        logger.debug 'searching for persisted matching SLA object'
-        logger.debug "ID=  #{daily_sla.id} DAY= #{daily_sla.day} expected day= #{day}"
-        if daily_sla.id != '' && (daily_sla.day == day)
-          logger.debug 'found match'
-          return daily_sla
-        end
-      end
-      logger.debug 'No match found creating new SlaPerDay and persisting'
-      return sla_per_day.call(sla_per_month)
-
-    end
-
-  end
 end
