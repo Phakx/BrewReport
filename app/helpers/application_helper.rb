36aea177086e2048dd11676e9a447ddcc9d16756
Bjoern Keucher <bjoern.keucher@web.de>
2014-03-18 14:42:42 +0100

    logging corrected

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -4,29 +4,28 @@ module ApplicationHelper
   require 'nokogiri'
 
   def self.import_downtimes_from_icinga(url, username, password, customer)
-    logger = Logger.new(STDOUT)
-    logger.debug 'Begin Fetching Icinga SLA Report'
+    Rails.logger.debug 'Begin Fetching Icinga SLA Report'
     xml = Nokogiri::XML(open(url, :http_basic_authentication => [username, password])) do |config|
       config.strict.nonet
     end
-    logger.debug 'Fetched XML starting to parse'
+    Rails.logger.debug 'Fetched XML starting to parse'
     log_entries = xml.xpath('//log_entry')
-    logger.debug "Found #{log_entries.size} Log entries"
+    Rails.logger.debug "Found #{log_entries.size} Log entries"
     log_entries.each do |entry|
-      logger.debug 'Creating new Downtime object'
-      create_new_downtime(entry, logger, customer)
+      Rails.logger.debug 'Creating new Downtime object'
+      create_new_downtime(entry, customer)
     end
   end
 
-  def self.create_new_downtime(entry, logger, customer)
+  def self.create_new_downtime(entry, customer)
     start_datetime = Time.at(entry.xpath('start_time_timestamp').text.to_i).to_datetime
     end_datetime = Time.at(entry.xpath('end_time_timestamp').text.to_i).to_datetime
     sla_per_day = find_or_create_sla_per_day(start_datetime.day, start_datetime.month, start_datetime.year, customer)
-    logger.debug 'Checking if Downtime already exists'
+    Rails.logger.debug 'Checking if Downtime already exists'
 
     if Downtime.check_if_downtime_exists(start_datetime, end_datetime).nil?
       downtime = Downtime.new
-      logger.debug "Using sla_per_day with id: #{sla_per_day.id} for new Downtime"
+      Rails.logger.debug "Using sla_per_day with id: #{sla_per_day.id} for new Downtime"
       downtime.sla_per_day = sla_per_day
       downtime.start = start_datetime
       downtime.end = end_datetime
@@ -34,12 +33,11 @@ module ApplicationHelper
       downtime.comment = entry.xpath('state_information').text
       downtime.save
     else
-      logger.debug("Downtime for #{start_datetime}  till   #{end_datetime}   already exists:")
+      Rails.logger.debug("Downtime for #{start_datetime}  till   #{end_datetime}   already exists:")
     end
   end
 
   def self.find_or_create_sla_per_day(day, month, year, customer)
-    logger = Logger.new(STDOUT)
     sla_per_day = lambda { |sla_per_month|
       daily_sla = SlaPerDay.new
       daily_sla.day=day
@@ -48,11 +46,11 @@ module ApplicationHelper
       return daily_sla
     }
 
-    logger.debug "Trying to find Sla_per_day object for #{day} / #{month} / #{year}"
+    Rails.logger.debug "Trying to find Sla_per_day object for #{day} / #{month} / #{year}"
     sla_per_month = SlaPerMonth.retrieve_by_month_and_year(month, year)
     if sla_per_month.nil?
 
-      logger.debug 'no matching month object found creating new Month and day object'
+      Rails.logger.debug 'no matching month object found creating new Month and day object'
       sla_per_month_new = SlaPerMonth.new
       sla_per_month_new.month= month
       sla_per_month_new.year = year
@@ -66,17 +64,17 @@ module ApplicationHelper
 
     else
 
-      logger.debug 'found sla_per_month object fetching day objects'
+      Rails.logger.debug 'found sla_per_month object fetching day objects'
       sla_per_days = SlaPerDay.retrieve_all_by_sla_per_month(sla_per_month.id)
       sla_per_days.each do |daily_sla|
-        logger.debug 'searching for persisted matching SLA object'
-        logger.debug "ID=  #{daily_sla.id} DAY= #{daily_sla.day} expected day= #{day}"
+        Rails.logger.debug 'searching for persisted matching SLA object'
+        Rails.logger.debug "ID=  #{daily_sla.id} DAY= #{daily_sla.day} expected day= #{day}"
         if daily_sla.id != '' && (daily_sla.day == day)
-          logger.debug 'found match'
+          Rails.logger.debug 'found match'
           return daily_sla
         end
       end
-      logger.debug 'No match found creating new SlaPerDay and persisting'
+      Rails.logger.debug 'No match found creating new SlaPerDay and persisting'
       return sla_per_day.call(sla_per_month)
 
     end
