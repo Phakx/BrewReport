0b49126ea9f14699826a29c980296dc4581dc349
wasabirox <wasabirox@googlemail.com>
2014-04-01 16:42:45 +0200

    ViewBootstrap

--- config/environments/test.rb
+++ config/environments/test.rb
@@ -1,6 +1,6 @@
 BrewReport::Application.configure do
   # Settings specified here will take precedence over those in config/application.rb.
-  config.less.compress = false
+  #config.less.compress = false
   # The test environment is used exclusively to run your application's
   # test suite. You never need to work with it otherwise. Remember that
   # your test database is "scratch space" for the test suite and is wiped
