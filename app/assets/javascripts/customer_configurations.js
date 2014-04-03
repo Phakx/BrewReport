075e7eb181d4b5d204cad03898902821c037a146
Bjoern Keucher <bjoern.keucher@web.de>
2014-04-03 16:46:20 +0200

    more multidatepicker

--- app/assets/javascripts/customer_configurations.js
+++ app/assets/javascripts/customer_configurations.js
@@ -1,2 +1,13 @@
 // Place all the behaviors and hooks related to the matching controller here.
 // All this logic will automatically be available in application.js.
+$(function() {
+    var altFieldDates = $('#altField').val();
+    var dates = altFieldDates === '' ? altFieldDates : altFieldDates.split(',');
+
+    $('#datepicker').multiDatesPicker({
+        dateFormat: "dd/mm/yy",
+        addDates: dates,
+        altField: '#altField'
+    });
+
+});
--- app/assets/javascripts/init.js
+++ app/assets/javascripts/init.js
@@ -1,6 +1,4 @@
 $('document').ready(function(){
 	$('#menu').Menu();
     $('.data_view').dataTable();
-    var today = new Date();
-    $('.datepicker').multiDatesPicker();
 });
